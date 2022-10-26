8d8995ef58d4b33f6a829577dbdd8c2dafa5c828
fr4ncisUCM <francisco.calvo.gonz@gmail.com>
2022-10-26 12:17:06 +0200

    Primer commit prueba

--- Practica1_1/mapper.py
+++ Practica1_1/mapper.py
@@ -4,12 +4,12 @@ import sys
 import re
 
 for line in sys.stdin:
-    file,line = line.split(":",1)
-    words = re.sub(r'\W+', ' ', line).split()
+    file, line = line.split(":", 1)
+    words = re.sub(r'\W+', ' ', line).split
+
     
     for word in words:
         print(word.lower() + "\t" + file)
-    ##
-
+#
 
 
--- Practica1_5/mapper5.py
+++ Practica1_5/mapper5.py
@@ -3,10 +3,10 @@ from datetime import datetime
 import sys
 
 import enum
-n=0
+n = 0
 
 for line in csv.reader(sys.stdin):
-    if n!= 0:   
+    if n != 0:
         print(line[3]+"\t"+(line[4] if line[4] != "" else "0"))
-    n=n+1
+    n = n+1
 
